5ce3a16aaedc9533fe75622698c0edc4df1082b8
ashbbdu <ashishsrivastava7007@gmail.com>
2025-09-04 23:53:45 +0530

    dashboard clone

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -1,7 +1,6 @@
-import { Injectable } from "@nestjs/common";
-import { User } from "./auth.model";
-import { InjectModel } from "@nestjs/sequelize";
-
+import { ConflictException, ForbiddenException, HttpException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { User } from './auth.model';
+import { InjectModel } from '@nestjs/sequelize';
 
 @Injectable()
 export class AuthRepository {
@@ -12,7 +11,35 @@ export class AuthRepository {
 
   async findRates() {
     return await this.authModel.findAll({
-        attributes : ['id' , "firstName"]
+      attributes: ['id', 'firstName'],
     });
   }
+
+  async register(data: any) {
+    const existingUser = await this.authModel.findByPk(data.email);
+    if(existingUser) {
+        throw new ConflictException({
+            status : "404",
+            message : "User already exist !"
+        });
+    }
+    const user = await this.authModel.create(data);
+    if(!user) {
+        throw new InternalServerErrorException();
+    }
+    return {
+        message : "User created successfully !",
+        user 
+    }
+  }
 }
+
+
+//  throw new HttpException({
+    //   status: HttpStatus.FORBIDDEN,
+    //   error: 'This is a custom message',
+//     }, HttpStatus.FORBIDDEN, {
+//       cause: error
+//     });
+//   }
+// }
