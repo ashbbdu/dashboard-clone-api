13393add7516f32c86971bef7518950b2d354685
ashbbdu <ashishsrivastava7007@gmail.com>
2025-09-06 00:20:27 +0530

    added dtos

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,24 +1,25 @@
 import { Body, Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDTO } from './dto/create-user.dto';
+import { LoginDTO } from './dto/login.dot';
 
 @Controller('auth')
 export class AuthController {
     constructor(
     private readonly authService: AuthService,
   ) {}
-    @Get("login")
+    @Get("rates")
     findRates () {
         return this.authService.findRates();
     }
-
+    @HttpCode(HttpStatus.CREATED)
     @Post("register")
     register (@Body() data : CreateUserDTO) {
         return this.authService.register(data);
     }
-     @HttpCode(HttpStatus.OK)
+    @HttpCode(HttpStatus.OK)
     @Post("login")
-    login (@Body() data : any) {
+    login (@Body() data : LoginDTO) {
     return this.authService.login(data);
   }
 }
