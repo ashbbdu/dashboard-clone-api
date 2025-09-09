b1732d817ac2c2984fdc2d5ec8d669f1f67b2322
ashbbdu <ashishsrivastava7007@gmail.com>
2025-09-09 23:01:17 +0530

    auth guard added , but need to inhance

--- src/quote/quote.controller.ts
+++ src/quote/quote.controller.ts
@@ -1,16 +1,20 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { QuoteService } from './quote.service';
-
+import { AuthGuard } from 'src/auth/auth.guard';
+@UseGuards(AuthGuard)
 @Controller('quote')
 export class QuoteController {
     constructor (private readonly quoteService : QuoteService) {}
+
     @Get("list") 
-    list () {
+    list (@Request() req) {
+        console.log(req.user , "user")
         return this.quoteService.list();
     }
 
     @Post("add")
-    add (@Body() data : any) {
+    add (@Body() data : any , @Request() req) {
+        console.log(req.user , "user from add")
         return this.quoteService.add(data);
     }
 }
